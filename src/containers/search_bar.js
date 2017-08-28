034d5c0c0823c1dcb1c55f20f39f00fd83ca17cc
Sharad Shinde <shindesharad71@gmail.com>
2017-08-28 15:56:11 +0530

    search bar text inpuy

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return(
             <form className="form-inline">
-                <input className="form-control col-md-4" type="text" aria-label="Search" placeholder="Search"/>
+                <input 
+                    className="form-control col-md-4" 
+                    type="text"  
+                    placeholder="Search City" 
+                    value={ this.state.term }
+                    onChange={this.onInputChange}
+                />
                 <button className="btn btn-outline-primary" type="submit">Search</button>
             </form>
         );
