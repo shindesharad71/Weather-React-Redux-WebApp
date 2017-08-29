2bacb20378fc916ea1895bf309ab344c44705b57
Sharad Shinde <shindesharad71@gmail.com>
2017-08-29 19:47:09 +0530

    table improved, units added

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import Charts from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humadities = cityData.list.map(weather => weather.main.humadity);
 
         return(
             <tr key={ name }>
                 <td>{ name }</td>
-                <td>
-                    <Sparklines height={120} width={180} data={ temps }>
-                        <SparklinesLine color="red"/>
-                    </Sparklines>
-                </td>
+                <td><Charts data={ temps } color="orange" unit="C"/></td>
+                <td><Charts data={ pressures } color="green" unit="hPa"/></td>
+                <td><Charts data={ humadities } color="blue" unit="%"/></td>
             </tr>
         );
     }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temprature</th>
-                        <th>Pressure</th>
-                        <th>Humadity</th>
+                        <th>Temprature (C)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humadity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
